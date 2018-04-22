fc771be1007ba8fc882d5205d4635bbe4e0d57ae
Jenalee Palmer <jenaleepalmer@gmail.com>
2018-04-22 14:10:39 -0500

    Add files via upload

---
+++ title/MainMenu.gd
@@ -0,0 +1,18 @@
+extends MarginContainer
+
+var volume_number = -80
+
+func _ready():
+	$ThemeSong.volume_db = volume_number
+	$ThemeSong/VolumeTimer.start()
+	pass
+
+func _on_VolumeTimer_timeout():
+	while volume_number < 0:
+		volume_number += 10
+		$ThemeSong.volume_db = volume_number
+		$ThemeSong/VolumeTimer.start()
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
