8cf15c8a4e25e563812751a6200e7d213da63f37
Jenalee Palmer <jenaleepalmer@gmail.com>
2018-04-22 23:47:35 -0600

    Added title song fade

--- title/MainMenu.gd
+++ title/MainMenu.gd
@@ -1,18 +1,6 @@
 extends CanvasLayer
 
-var volume_number = -80
-
 func _ready():
-	$ThemeSong.volume_db = volume_number
-	$ThemeSong/VolumeTimer.start()
+	$ThemeSong/fade.play("song_fade")
 	pass
 
-func _on_VolumeTimer_timeout():
-	while volume_number < 0:
-		volume_number += 10
-		$ThemeSong.volume_db = volume_number
-		$ThemeSong/VolumeTimer.start()
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
