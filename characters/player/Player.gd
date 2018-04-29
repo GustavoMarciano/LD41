ed765448a96bb9699c173f7d0fbf544a81996c9e
Gustavo Marciano <gustavodexpace@gmail.com>
2018-04-29 00:22:03 -0300

    I cleaned the player controls

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -1,44 +1,33 @@
 extends KinematicBody2D
 
+const GRAVITY = 1
+const SPEED = 50
+
 var prev_frame = 0
-
-var dir= Vector2()
-var walk = Vector2()
-
-var left = Vector2(-1,0)
-var right = Vector2(1,0)
-var up = Vector2(0,-1)
-var down = Vector2(0,1)
-var stop = Vector2(0,0)
-
-func _ready():
-	pass
+var motion = Vector2()
+const up = Vector2(0,-1)
 	
 func _physics_process(delta):
-	if prev_frame != $icon.frame:
-		if $icon.frame == 2:
-			$r_foot.play()
-		if $icon.frame == 5:
-			$l_foot.play()
-		prev_frame = $icon.frame
-
-	walk = dir * 50
-	walk = move_and_slide(walk,up)
-	position.y += 1 
-
+	sound()
+	motion.y += GRAVITY 
+	motion = move_and_slide(motion,up)
 
 func move(direction):
 	match direction:
 		"left":
-			dir = left
+			motion.x = -SPEED
 			$Animations.play("Walk Left")
 		"right":
-			dir = right
+			motion.x = SPEED
 			$Animations.play("Walk Right")
-		"up":
-			dir = up
-		"down":
-			dir = down
 		"stop":
-			dir = stop
+			motion.x = 0
 			$Animations.stop()
+
+func sound():
+		if prev_frame != $icon.frame:
+			if $icon.frame == 2:
+				$r_foot.play()
+			if $icon.frame == 5:
+				$l_foot.play()
+			prev_frame = $icon.frame
