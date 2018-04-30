e5abbfddab134e18e9e91a03645c2ac622fa8a26
Jenalee Palmer <jenaleepalmer@gmail.com>
2018-04-30 03:48:49 -0500

    Multiple Changes
    Changed title screen contents to sprites, added "press enter to play" with animation, added credits sprite, added escape function (press esc to exit game)

---
+++ interface/Escape.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _unhandled_input(event):
+    if event is InputEventKey:
+        if event.pressed and event.scancode == KEY_ESCAPE:
+            get_tree().quit()
--- title/MainMenu.gd
+++ title/MainMenu.gd
@@ -2,5 +2,10 @@ extends CanvasLayer
 
 func _ready():
 	$ThemeSong/fade.play("song_fade")
+	$PlayText/AnimationPlayer.play("shine")
 	pass
 
+func _unhandled_input(event):
+    if event is InputEventKey:
+        if event.pressed and event.scancode == KEY_ENTER:
+            get_tree().change_scene("res://levels/Ship/Ship.tscn")
