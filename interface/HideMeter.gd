5f2130e15721508804eeb70f8bb88ac4086c5c5d
Jenalee Palmer <jenaleepalmer@gmail.com>
2018-05-01 01:37:02 -0500

    Uhh...Read more
    I'm not sure if I did naything really. But there were a couple changes, so I'm pushing them.

--- interface/HideMeter.gd
+++ interface/HideMeter.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var HideAbility = 1 #1 for able, 0 for not able
+onready var hud = $"../../Player/HUD/Top/Panel/RichTextLabel"
 
 func _ready():
 	pass
@@ -12,6 +13,12 @@ func _on_AnimationPlayer_animation_started(HideMeter):
 func _on_AnimationPlayer_animation_finished(anim_name):
 	HideAbility = 1
 	pass
+	
+func hide():
+	if HideAbility == 0:
+		pass
+	if HideAbility == 1:
+		$MeterLine/AnimationPlayer.play("HideMeter")
 
 func _unhandled_input(event):
 	if event is InputEventKey:
